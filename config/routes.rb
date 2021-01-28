74edd41cefaaafc7b3a812a00c5d36688661df9b
Michael Fuget <michaelfuget@gmail.com>
2021-01-28 07:25:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
     # nested resource for posts
     resources :posts, only: [:show, :index]
   end
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> 6a789d881e11690a3583f3b01bf93a1beeab61a4
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-
+ 
   root 'posts#index'
 
 end
